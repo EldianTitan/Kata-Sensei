c976bef13e331d140afc975ba8046b5874b202b8
EldianTitan <jason.philippou@gmail.com>
2020-09-16 00:51:01 +0300

    Added newkata command.

---
+++ commands/newkata.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+
+//member.roles.cache.some(role => role.name === 'Mod');
+function execute(message, args, user_data) {
+    const is_moderator = message.member.roles.cache.some(role => role.name.toLowerCase().startsWith('moderator'));
+    const is_admin = message.member.hasPermission(['ADMINISTRATOR']);
+    
+    if (!is_moderator && !is_admin) {
+        message.channel.send("Hmmm, adequate permission you have not! Moderator you must be."); //Get it? Its Yoda.
+        return;
+    }
+
+    if (args.length != 3) {
+        message.channel.send("I don't understand the command. This is the correct syntax:" +
+                             "`!sensei newkata <kata_level> <kata_id_or_slug>`");
+        return;
+    }
+
+    if (args[1] < 1 || args[1] > 4) {
+        message.channel.send("Wow, that Kata level doesn't seem right! Kata levels must be between 1 and 4 (inclusive).");
+        return;
+    }
+
+    axios.get(`https://www.codewars.com/api/v1/code-challenges/${args[2]}`)
+        .then(response => {
+            message.channel.send(`Kata ${args[1]} has been set to \`${response.data.name}\`!`);
+
+            //Kata list does not exist
+            if (user_data["kata_list"] == null) {
+                user_data["kata_list"] = [];
+            }
+
+            user_data["kata_list"][args[1] - 1] = { "name": response.data.name, "id": args[2] };
+        })
+        .catch(error => {
+            if (error.response.status == 404) {
+                message.channel.send("The kata provided was not found!");
+            } else {
+                message.channel.send(`Ooops, an error occured (${error.response.status})!`);
+            }
+        });
+}
+
+module.exports = {
+    name: "newkata",
+    callback: execute
+}
