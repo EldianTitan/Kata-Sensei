33b99fdcf4f35165629d72a94a09658bddc2e936
LawtherJacob Lawther <b9035128@ncl.ac.uk>
2020-09-21 12:03:52 +0100

    Created js file help with simple message function

--- commands/help.js
+++ commands/help.js
@@ -2,11 +2,6 @@ function execute(message, args, user_data) {
     const is_moderator = message.member.roles.cache.some(role => role.name.toLowerCase().startsWith('moderator'));
     const is_admin = message.member.hasPermission(['ADMINISTRATOR']);
 
-    if (!is_moderator && !is_admin) {
-        message.channel.send("Hmmm, adequate permission you have not! Moderator you must be."); //Get it? Its Yoda.
-        return;
-        }
-
         const Discord = require('discord.js');
 
         // inside a command, event listener, etc.
@@ -14,26 +9,37 @@ function execute(message, args, user_data) {
         	.setColor('#FF0000')
         	.setTitle('Kata Bot Help Commands')
         	.setURL('https://discord.js.org/')
-        	.setAuthor('Bruce Lee', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-        	.setDescription('Bruce Lee is a really cool guy that made this kata bot so you can be as cool as him')
+        	.setAuthor('Jiagi', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+        	.setDescription('Jiagi is a really cool guy that made this kata bot so you can be as cool as GunGuy')
         	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
         	.addFields(
         		{ name: 'Commands', value: 'On the menu today is newkata, registerme and scanme there is also'+
         		 ' a help command but I think you already know that.'},
         		{ name: '\u200B', value: '\u200B' },
         		{ name: 'newkata', value: 'The way to use this command is ' +
-        		'`!sensei newkata <kata_level> <kata_id_or_slug>`' + 'this command is for assigning new katas'
-        		, inline: true },
+        		'`!sensei newkata <kata_level> <kata_id_or_slug>`' + ' this command is for assigning new katas'
+        		, inline: false },
+        		{ name: '\u100B', value: '\u100B' },
+        		{ name: 'registerme', value: 'The way to use this command is ' +
+                        		'`!sensei registerme <codewars_username>`' +
+                        	    ' this command is for registering yourself for scans'
+                        		, inline: false },
+                { name: '\u100B', value: '\u100B' },
+                { name: 'scanme', value: 'The way to use this command is ' +
+                              '`!sensei scaneme`' +
+                              ' this command is for scanning and giving yourself roles REMEMBER TO REGISTER FIRST'
+                                        		, inline: false }
         	)
-        	.addField('Inline field title', 'Some value here', true)
         	.setImage('https://i.imgur.com/wSTFkRM.png')
         	.setTimestamp()
-        	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+        	.setFooter('Earth was here', 'https://i.imgur.com/wSTFkRM.png');
 
-        channel.send(exampleEmbed);
+        message.channel.send(exampleEmbed);
     }
 
 module.exports = {
     name: "help",
+    needs_privilege: false,
+    channel_name: "general",
     callback: execute
 }
